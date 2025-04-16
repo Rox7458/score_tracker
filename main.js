9c0f19260a1b53ded2ea22ce807c66807623cf83
Rox <jagaaoguz.7000@gmail.com>
2025-04-16 11:10:24 +0300

    key verification added

--- main.js
+++ main.js
@@ -10,18 +10,22 @@ const btnYellow = document.querySelector(".btn-yellow");
 const btnBlue = document.querySelector(".btn-blue");
 const btnRed = document.querySelector(".btn-red");
 
+let key = true;
+
 // increase button of player 1
 btnYellow.addEventListener("click", () => {
-  user1.innerText = Number(user1.innerText) + 1;
-
-  judge();
+  if (key) {
+    user1.innerText = Number(user1.innerText) + 1;
+    judge();
+  }
 });
 
 // increase button of player 2
 btnBlue.addEventListener("click", () => {
-  user2.innerText = Number(user2.innerText) + 1;
-
-  judge();
+  if (key) {
+    user2.innerText = Number(user2.innerText) + 1;
+    judge();
+  }
 });
 
 // reset button
@@ -30,6 +34,7 @@ btnRed.addEventListener("click", () => {
   user2.innerText = 0;
   user1.setAttribute("style", "color:black");
   user2.setAttribute("style", "color:black");
+  key = true;
 });
 
 // judge will determine who win
@@ -37,10 +42,12 @@ const judge = () => {
   if (input.value == Number(user1.innerText) && input.value != "") {
     user1.setAttribute("style", "color:green");
     user2.setAttribute("style", "color:red");
+    key = false;
   }
 
   if (input.value == Number(user2.innerText) && input.value != "") {
     user2.setAttribute("style", "color:green");
     user1.setAttribute("style", "color:red");
+    key = false;
   }
 };
